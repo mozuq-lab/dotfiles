833ad774ef06ed887dd94a4c274351056cd2131a
pepo-le <hello@pepo-le.com>
2017-03-12 02:00:35 +0900

    setup.sh Mac用の修正など

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
+    [ "$f" = ".DS_Store" ] && continue
+    [ "$f" = ".desktop" ] && continue
 
     ln -s "$HOME"/dotfiles/"$f" "$HOME"/"$f"
 done
