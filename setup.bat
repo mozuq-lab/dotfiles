cdc6b665e5dbedd39c726df483db9a686658228a
pepo-le <hello@pepo-le.com>
2017-03-31 16:22:07 +0900

    setup.bat, setup.sh Windows以外では.nyagosのリンクを作成しないようした

--- setup.bat
+++ setup.bat
@@ -1,13 +1,11 @@
 @echo off
 for /d %%i in (.*) do (
-	if not %%i == .git if not %%i == .DS_Store (
+	if not %%i == .git (
 		mklink /d %USERPROFILE%\%%i %USERPROFILE%\dotfiles\%%i
 	)
 )
 for %%i in (.*) do (
-	if not %%i == .desktop (
-		mklink %USERPROFILE%\%%i %USERPROFILE%\dotfiles\%%i
-	)
+	mklink %USERPROFILE%\%%i %USERPROFILE%\dotfiles\%%i
 )
 
 mkdir %USERPROFILE%\.vim\backup
--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".DS_Store" ] && continue
     [ "$f" = ".desktop" ] && continue
+    [ "$f" = ".nyagos" ] && continue
 
     ln -s "$HOME"/dotfiles/"$f" "$HOME"/"$f"
 done
